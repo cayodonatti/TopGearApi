a4ea2d2bd49e95becb515e399b84708d2c07131c
Cayo Paulo Donatti <donatti.dev@gmail.com>
2017-09-16 13:20:06 -0300

    Removido atributo da classe response

--- TopGearApi/Models/Response.cs
+++ TopGearApi/Models/Response.cs
@@ -8,7 +8,6 @@ namespace TopGearApi.Models
     public class Response<T>
     {
         public bool Sucesso { get; set; }
-        private bool HasDados { get => Dados != null; }
 
         public T Dados { get; set; }
     }
