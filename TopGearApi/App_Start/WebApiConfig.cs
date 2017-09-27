23f0f196d362cbc00a36401e08119a581c5a7ee0
Cayo Paulo Donatti <donatti.dev@gmail.com>
2017-09-27 14:56:50 -0300

    Update WebApiConfig.cs

--- TopGearApi/App_Start/WebApiConfig.cs
+++ TopGearApi/App_Start/WebApiConfig.cs
@@ -13,13 +13,13 @@ namespace TopGearApi
 
             // Web API routes
             config.MapHttpAttributeRoutes();
-
+            /*
             config.Routes.MapHttpRoute(
                 name: "ActionApi",
                 routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
-
+            */
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
