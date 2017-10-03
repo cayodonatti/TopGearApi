37dc119a69c6ac64674c24e8d91886a61969242a
Cayo Paulo Donatti <donatti.dev@gmail.com>
2017-10-03 20:22:10 -0300

    Ajustes em propriedades do projeto e filtros

---
+++ TopGearApi/Models/RequestCarrosDisponiveis.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using TopGearApi.Domain.Models;
+
+namespace TopGearApi.Models
+{
+    public class RequestCarrosDisponiveis : Request<List<Carro>>
+    {
+        [Required]
+        public DateTime Inicial { get; set; }
+        [Required]
+        public DateTime Final { get; set; }
+        public int? AgenciaId { get; set; }
+    }
+}
