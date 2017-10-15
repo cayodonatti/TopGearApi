302657d519fc8bbd68f56ffe0a52b3db81745875
Cayo Paulo Donatti <donatti.dev@gmail.com>
2017-10-15 16:18:43 -0200

    Concluindo teste de locação

--- TopGearApi.Test/LocacaoControllerTest.cs
+++ TopGearApi.Test/LocacaoControllerTest.cs
@@ -21,31 +21,31 @@ namespace TopGearApi.Test
         [TestMethod]
         public void Post_Locacao()
         {
-            //var req = new RequestCarrosDisponiveis
-            //{
-            //    Inicial = new DateTime(2017, 10, 01),
-            //    Final = new DateTime(2017, 10, 15),
-            //    Token = TopGearApi<Carro>.GetToken()
-            //};
+            var req = new RequestCarrosDisponiveis
+            {
+                Inicial = new DateTime(2017, 10, 01),
+                Final = new DateTime(2017, 10, 15),
+                Token = TopGearApi<Carro>.GetToken()
+            };
 
-            //List<Carro> carros = CarroApi.ObterDisponiveis(req).Dados;
-            //Locacao l = new Locacao
-            //{
-            //    CarroId = carros.First().Id,
-            //    ClienteId = TopGearApi<List<Cliente>>.Get("cliente").Dados.First().Id,
-            //    Agencia_EntregaId = TopGearApi<List<Agencia>>.Get("agencia").Dados.First().Id,
-            //    Agencia_RetiradaId = TopGearApi<List<Agencia>>.Get("agencia").Dados.First().Id,
-            //    Retirada = new DateTime(2017, 10, 15),
-            //    Entrega = new DateTime(2017, 10, 20)
-            //};
+            List<Carro> carros = CarroApi.ObterDisponiveis(req).Dados;
+            Locacao l = new Locacao
+            {
+                CarroId = carros.First().Id,
+                ClienteId = TopGearApi<List<Cliente>>.Get("cliente").Dados.First().Id,
+                Agencia_EntregaId = TopGearApi<List<Agencia>>.Get("agencia").Dados.First().Id,
+                Agencia_RetiradaId = TopGearApi<List<Agencia>>.Get("agencia").Dados.First().Id,
+                Retirada = new DateTime(2017, 10, 15),
+                Entrega = new DateTime(2017, 10, 20)
+            };
 
-            //var Id = this.Post(l);
+            var Id = this.Post(l);
 
-            //Assert.IsFalse(l.CarroId == carros.Last().Id);
-            //l.CarroId = carros.Last().Id;
-            //this.Update(Id, l);
+            Assert.IsFalse(l.CarroId == carros.Last().Id);
+            l.CarroId = carros.Last().Id;
+            this.Update(Id, l);
 
-            //this.Delete(Id);
+            this.Delete(Id);
         }
     }
 }
