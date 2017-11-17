6dc68156ecaa27b60e7e2a79e70324790931ad73
Cayo Paulo Donatti <donatti.dev@gmail.com>
2017-11-17 10:16:44 -0200

    Adicionado email de cliente, criada API de acesso à passagens aéreas

---
+++ TopGearApi.Access/PassagemModels/EnviarPassageiro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopGearApi.Access.PassagemModels
+{
+    public class EnviarPassageiro
+    {
+        public string nome { get; set; }
+        public string cpf { get; set; }
+        public DateTime? dataNascimento { get; set; }
+    }
+}
